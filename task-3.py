1a68f4c3bbd4302144528fd9322a3904f31032b2
tyler-barsby <tyler@flowmondo.com>
2025-11-27 14:16:56 +0000

    updated task-3.py

--- task-3.py
+++ task-3.py
@@ -24,15 +24,15 @@ def weightconverter():
             if selected_unit == "Kgs":
                 weight = float(input(f"\nPlease enter a weight in Lbs: "))
                 
-                calculated_pounds = weight * 2.20462
+                calculated_pounds = weight / 2.20462
 
-                return f"\n{weight}Lbs in {selected_unit} is:\n\t{weight} * 2.20462 = {calculated_pounds:.3f} Kgs"
+                return f"\n{weight}Lbs in {selected_unit} is:\n\t{weight} / 2.20462 = {calculated_pounds:.3f} Kgs"
             elif selected_unit == "Lbs":
                 weight = float(input(f"\nPlease enter a weight in Kgs: "))
                 
-                calculated_kilos = weight / 2.20462
+                calculated_kilos = weight * 2.20462
 
-                return f"\n{weight}Kgs in {selected_unit} is:\n\t{weight} / 2.20462 = {calculated_kilos:.3f} Lbs"
+                return f"\n{weight}Kgs in {selected_unit} is:\n\t{weight} * 2.20462 = {calculated_kilos:.3f} Lbs"
         else:
             return f"\nError: Please select a valid number from the menu."
             
