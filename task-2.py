65c03201dce3c6cc6b8793ad5b64b71f977cb0df
tyler-barsby <tyler@flowmondo.com>
2025-11-27 11:33:42 +0000

    task-2.py created with json style dictionary structure

---
+++ task-2.py
@@ -0,0 +1,81 @@
+library = {
+    "J. R. R. Tolkein": [
+        {
+            "name": "The Hobbit",
+            "publisher": "Harper Collins",
+            "year": 1937,
+            "ISBN": 1127846,
+            "genre": "Fantasy",
+            "type": "Fiction"
+        },
+        {
+            "name": "The Lord Of The Rings",
+            "publisher": "Harper Collins",
+            "year": 1957,
+            "ISBN": 817268376,
+            "genre": "Fantasy",
+            "type": "Fiction"
+        },
+        {
+            "name": "The Fellowship Of The Ring",
+            "publisher": "Harper Collins",
+            "year": 1954,
+            "ISBN": 1012930983,
+            "genre": "Fantasy",
+            "type": "Fiction"
+        }
+    ],
+    "Michael Morpurgo": [
+        {
+            "name": "War Horse",
+            "publisher": "Ladybird books",
+            "year": 1982,
+            "ISBN": 9786543210987,
+            "genre": "Children's Fiction",
+            "type": "Fiction"
+        },
+        {
+            "name": "Private Peaceful",
+            "publisher": "Ladybird books",
+            "year": 2003,
+            "ISBN": 6543567890123,
+            "genre": "Children's Fiction",
+            "type": "Fiction"
+        },
+        {
+            "name": "Born To Run",
+            "publisher": "Ladybird books",
+            "year": 2007,
+            "ISBN": 453678908765,
+            "genre": "Children's Fiction",
+            "type": "Fiction"
+        }
+    ],
+    "J. K. Rowling": [
+        {
+            "name": "Harry Potter: The Philosophers Stone",
+            "publisher": "Penguin Random House",
+            "year": 1997,
+            "ISBN": 98765435876543,
+            "genre": "Fantasy",
+            "type": "Fiction"
+        },
+        {
+            "name": "Harry Potter: The Chamber Of Secrets",
+            "publisher": "Penguin Random House",
+            "year": 1998,
+            "ISBN": 19876546986,
+            "genre": "Fantasy",
+            "type": "Fiction"
+        },
+        {
+            "name": "Harry Potter: The Prisoner Of Azkaban",
+            "publisher": "Penguin Random House",
+            "year": 1999,
+            "ISBN": 3456789087654,
+            "genre": "Fantasy",
+            "type": "Fiction"
+        }
+    ]
+}
+
