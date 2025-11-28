bd72b5e2a225cd2cc2efcbc7e9f1654f49998ab2
tyler-barsby <tyler@flowmondo.com>
2025-11-28 14:34:36 +0000

    Conversion to hours an minutes added

--- task-7.py
+++ task-7.py
@@ -22,12 +22,10 @@ def get_valid_time_input(prompt):
 
 def add_times():
     print(">> Executing: Add 2 Times")
-    time.sleep(1)
+    time.sleep(0.5)
     
     date1 = get_valid_time_input(f"Please enter a date to add (DD:HH:MM): ")
     date2 = get_valid_time_input(f"Please another date to add to {date1} (DD:HH:MM): ")
-    
-    # print(f"Accepted inputs: {date1} and {date2}") # debugging step
     
     date1_day = int(date1.split(":")[0])
     date1_hours = int(date1.split(":")[1])
@@ -37,34 +35,65 @@ def add_times():
     date2_hours = int(date2.split(":")[1])
     date2_mins = int(date2.split(":")[-1])
     
-    calculated_date = f"{date1_day + date2_day}:{date1_hours + date2_hours}:{date1_mins + date2_mins}"
+    calculated_date = f"{date1_day + date2_day}:{date1_hours + date2_hours}:{date1_mins + date2_mins}\n"
     
     return calculated_date
 
     
 def diff_times():
     print(">> Executing: Difference Of 2 Times")
-    time.sleep(1)
+    time.sleep(0.5)
+    date1 = get_valid_time_input(f"Please enter a date (DD:HH:MM): ")
+    date2 = get_valid_time_input(f"Please another date (DD:HH:MM): ")
+    
+    date1_day = int(date1.split(":")[0])
+    date1_hours = int(date1.split(":")[1])
+    date1_mins = int(date1.split(":")[-1])
+    
+    date2_day = int(date2.split(":")[0])
+    date2_hours = int(date2.split(":")[1])
+    date2_mins = int(date2.split(":")[-1])
+    
+    calculated_date = f"{date1_day - date2_day}:{date1_hours - date2_hours}:{date1_mins - date2_mins}\n"
+    
+    return calculated_date
 
 def convert_to_hours():
     print(">> Executing: Convert To Hours")
-    time.sleep(1)
+    time.sleep(0.5)
+    date = get_valid_time_input(f"Please enter a date to convert to hours (DD:HH:MM): ")
+    
+    date_day = int(date.split(":")[0])
+    date_hours = int(date.split(":")[1])
+    date_mins = int(date.split(":")[-1])
+    
+    calculated_date = f"{((date_day * 24) + date_hours + (date_mins / 60)):.2f} hours"
+    return calculated_date
 
 def convert_to_minutes():
     print(">> Executing: Convert To Minutes")
-    time.sleep(1)
+    time.sleep(0.5)
+
+    date = get_valid_time_input(f"Please enter a date to add (DD:HH:MM): ")
+    
+    date_day = int(date.split(":")[0])
+    date_hours = int(date.split(":")[1])
+    date_mins = int(date.split(":")[-1])
+    
+    calculated_date = f"{((date_day * 1440) + (date_hours * 60) + (date_mins)):.2f} minutes"
+    return calculated_date
 
 def convert_minutes_to_time():
     print(">> Executing: Convert Minutes To Time")
-    time.sleep(1)
+    time.sleep(0.5)
 
 def convert_hours_to_time():
     print(">> Executing: Convert Hours To Time")
-    time.sleep(1)
+    time.sleep(0.5)
 
 def convert_days_to_time():
     print(">> Executing: Convert Days To Time")
-    time.sleep(1)
+    time.sleep(0.5)
 
 def exit_script():
     print("Exiting program...")
@@ -116,6 +145,7 @@ def main():
         if 1 <= choice <= len(menu_actions):
             selected_function = menu_actions[choice - 1] 
             print(selected_function())
+            input("Please press enter to CONTINUE")
         else:
             print(f"\n[ERROR]: Please enter a number between 1 and {len(menu_actions)}.")
 
